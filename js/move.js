5454a204013eac164e6b51cc5e09b42b2264be6b
Lee.Hyunsup <clocktree@live.co.kr>
2020-02-01 00:48:24 +0900

    20200201_2
    - 수정.

--- js/move.js
+++ js/move.js
@@ -2405,20 +2405,20 @@ $(function(){
 				setTimeout(lazy_0,0);
 				if($('#slide-wrap').find('.controll').length<1){
 					$('#slide-wrap').append('<span class="timebar" style="display:inline-block;position:absolute;top:0px;left:0;width:0;height:30px;background:rgba(0,0,0,0.7);z-index:1"></span>')
-					$('.timebar').stop().animate({'width':'(calc = 100%-120px)'},barspeed);
+					$('.timebar').stop().animate({'width':'calc(100%-120px)'},barspeed);
 					bar_on = setInterval(function(){
 							$('.timebar').remove();
 							$('#slide-wrap').append('<span class="timebar" style="display:inline-block;position:absolute;top:0px;left:0;width:0;height:30px;background:rgba(0,0,0,0.7);z-index:1"></span>')
-							$('.timebar').stop().animate({'width':'(calc = 100%-120px)'},barspeed);
+							$('.timebar').stop().animate({'width':'calc(100%-120px)'},barspeed);
 					},autospeed);
 				}else{
 					if($('.controll input[type=checkbox]').prop('checked')==false){
 						$('#slide-wrap').append('<span class="timebar" style="display:inline-block;position:absolute;top:0px;left:0;width:0;height:30px;background:rgba(0,0,0,0.7);z-index:1"></span>')
-						$('.timebar').stop().animate({'width':'(calc = 100%-120px)'},barspeed);
+						$('.timebar').stop().animate({'width':'calc(100%-120px)'},barspeed);
 						bar_on = setInterval(function(){
 								$('.timebar').remove();
 								$('#slide-wrap').append('<span class="timebar" style="display:inline-block;position:absolute;top:0px;left:0;width:0;height:30px;background:rgba(0,0,0,0.7);z-index:1"></span>')
-								$('.timebar').stop().animate({'width':'(calc = 100%-120px)'},barspeed);
+								$('.timebar').stop().animate({'width':'calc(100%-120px)'},barspeed);
 						},autospeed);
 					}
 				}
