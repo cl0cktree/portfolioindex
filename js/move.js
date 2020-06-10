1ca084b116ef718cf468426b5eb2c75ff433caf2
Lee.Hyunsup <clocktree@live.co.kr>
2020-06-10 19:30:33 +0900

    20200610_4
    - 수정.

--- js/move.js
+++ js/move.js
@@ -2001,7 +2001,7 @@ $(function(){
 				var tempt = json.main.temp;
 				var place = json.name;
 				var weather_this = json.weather[0].main;
-				$('.filter-landing-contents').append('<div class="cover-city">'+place+' : '+tempt+'℃ '+weather_this+'</div>');
+				$('.filter-landing-contents').append('<div class="cover-city">'+place+' : '+tempt+'℃ / '+weather_this+'</div>');
 		    });
 		};
 		weather_json();
